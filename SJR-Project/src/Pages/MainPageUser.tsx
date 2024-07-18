1616239a278a96a658f94d3f3b116f01a54912fb
Muhammad Faried Gunawan <fariedmuhammad04@gmail.com>
2024-07-18 16:18:16 +0700

    update table

--- SJR-Project/src/Pages/MainPageUser.tsx
+++ SJR-Project/src/Pages/MainPageUser.tsx
@@ -1,84 +1,109 @@
 import SidebarUser from "../component/SidebarUser";
+import React, { useState } from "react";
 import { Menu, MenuHandler, Button, MenuList, MenuItem, Checkbox, Input } from "@material-tailwind/react";
-import { PencilIcon } from "@heroicons/react/24/solid";
-import { ArrowDownTrayIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Card, CardHeader, Typography, CardBody, Chip, CardFooter, Avatar, IconButton, Tooltip } from "@material-tailwind/react";
-
-const TABLE_HEAD = ["Product Name", "Brand", "Category", "Price", "Status", "Quantity", ""];
-
-const TABLE_ROWS = [
+const products = [
   {
     name: "Mikrotik RB951UI2ND",
     brand: "Mikrotik",
     category: "Router",
-    price: "Rp.750.000/pcs",
-    status: "ready",
-    quantity: "1900",
+    price: "Rp.760.000",
+    status: "Ready",
+    quantity: 180,
   },
   {
-    name: "Mikrotik RB951UI2ND",
-    brand: "Mikrotik",
-    category: "Router",
-    price: "Rp.750.000/pcs",
-    status: "ready",
-    quantity: "1900",
+    name: "Ubiquiti NanoStation M2",
+    brand: "Ubiquiti",
+    category: "Access Point",
+    price: "Rp.1.200.000",
+    status: "Ready",
+    quantity: 100,
   },
   {
-    name: "Mikrotik RB951UI2ND",
-    brand: "Mikrotik",
+    name: "TP-Link TL-WR840N",
+    brand: "TP-Link",
     category: "Router",
-    price: "Rp.750.000/pcs",
-    status: "soldout",
-    quantity: "1900",
+    price: "Rp.250.000",
+    status: "Ready",
+    quantity: 250,
   },
   {
-    name: "Mikrotik RB951UI2ND",
-    brand: "Mikrotik",
+    name: "TP-Link TL-WR840N",
+    brand: "TP-Link",
     category: "Router",
-    price: "Rp.750.000/pcs",
-    status: "ready",
-    quantity: "1900",
+    price: "Rp.250.000",
+    status: "Ready",
+    quantity: 250,
   },
   {
-    name: "Mikrotik RB951UI2ND",
-    brand: "Mikrotik",
+    name: "TP-Link TL-WR840N",
+    brand: "TP-Link",
     category: "Router",
-    price: "Rp.750.000/pcs",
-    status: "ready",
-    quantity: "1900",
+    price: "Rp.250.000",
+    status: "Ready",
+    quantity: 250,
   },
   {
-    name: "Mikrotik RB951UI2ND",
-    brand: "Mikrotik",
+    name: "TP-Link TL-WR840N",
+    brand: "TP-Link",
     category: "Router",
-    price: "Rp.750.000/pcs",
-    status: "ready",
-    quantity: "1900",
+    price: "Rp.250.000",
+    status: "Ready",
+    quantity: 250,
+  },
+  {
+    name: "TP-Link TL-WR840N",
+    brand: "TP-Link",
+    category: "Router",
+    price: "Rp.250.000",
+    status: "Ready",
+    quantity: 250,
+  },
+  {
+    name: "TP-Link TL-WR840N",
+    brand: "TP-Link",
+    category: "Router",
+    price: "Rp.250.000",
+    status: "Soldout",
+    quantity: 0,
+  },
+  {
+    name: "TP-Link TL-WR840N",
+    brand: "TP-Link",
+    category: "Router",
+    price: "Rp.250.000",
+    status: "Ready",
+    quantity: 250,
+  },
+  {
+    name: "TP-Link TL-WR840N",
+    brand: "TP-Link",
+    category: "Router",
+    price: "Rp.250.000",
+    status: "Ready",
+    quantity: 250,
   },
 ];
-
 const MainPageUser = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredProducts = products.filter((product) => product.name.toLowerCase().includes(searchTerm.toLowerCase()));
   return (
-    <div className="body-main bg-[#F2F2F2]">
+    <div className="body-main bg-[#F2F2F2] overflow-hidden">
       <div className="container-main flex flex-row min-h-screen">
         <div className="side-bar">
           <SidebarUser />
         </div>
 
         <div className="content-container flex flex-col ml-[35px] mt-[38px]">
-          {/*header*/}
+          {/* header */}
           <div className="main-menu flex">
             <h2 className="font-bold text-[30px]">Product</h2>
           </div>
 
-          {/*search engine*/}
+          {/* search engine */}
           <div className="search flex flex-row items-center gap-[80px] mt-[30px]">
             <div className="category">
-              <Menu
-                dismiss={{
-                  itemPress: false,
-                }}
-              >
+              <Menu dismiss={{ itemPress: false }}>
                 <MenuHandler>
                   <Button className="bg-white px-[50px] py-[7px] text-black text-[14px] capitalize font-normal rounded-2xl">Category</Button>
                 </MenuHandler>
@@ -105,11 +130,7 @@ const MainPageUser = () => {
               </Menu>
             </div>
             <div className="brand">
-              <Menu
-                dismiss={{
-                  itemPress: false,
-                }}
-              >
+              <Menu dismiss={{ itemPress: false }}>
                 <MenuHandler>
                   <Button className="bg-white px-[60px] py-[7px] text-black text-[14px] capitalize font-normal rounded-2xl">Brand</Button>
                 </MenuHandler>
@@ -143,107 +164,46 @@ const MainPageUser = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                   </svg>
                 }
-                className="bg-white border-none rounded-[200px] w-[400px] shadow-md text-black"
+                className="bg-white border-none rounded-[400px] w-[350px] shadow-md text-black"
+                onChange={(e) => setSearchTerm(e.target.value)}
               />
             </div>
           </div>
-          <div className="table-of-content mt-[10px]">
-            <Card className="h-full w-[1200px] bg-transparent shadow-none">
-              <CardBody className="px-0">
-                <table className="w-full min-w-max table-auto text-left">
-                  <thead>
-                    <tr>
-                      {TABLE_HEAD.map((head) => (
-                        <th key={head} className="border-y-none border-blue-gray-100 bg-blue-gray-50/50 p-4">
-                          <Typography variant="small" color="blue-gray" className="font-normal leading-none opacity-70">
-                            {head}
-                          </Typography>
-                        </th>
-                      ))}
+          <div className="table-of-content mt-[40px]">
+            <table className="text-left w-[1200px]">
+              <thead>
+                <tr className="text-[16px]">
+                  <th className="font-medium text-[#989898] w-[17rem]">Product Name</th>
+                  <th className="font-medium text-[#989898] w-[10rem]">Brand</th>
+                  <th className="font-medium text-[#989898] w-[12rem]">Category</th>
+                  <th className="font-medium text-[#989898] w-[10rem]">Price</th>
+                  <th className="font-medium text-[#989898] w-[10rem]">Status</th>
+                  <th className="font-medium text-[#989898] w-[10rem]">Quantity</th>
+                  <th className="font-medium text-[#F2F2F2] w-[10rem]">Actions</th>
+                </tr>
+              </thead>
+            </table>
+            <div className="overflow-y-scroll h-[31rem] mt-[20px]">
+              <table className="text-left w-[1200px]">
+                <tbody>
+                  {filteredProducts.map((product, index) => (
+                    <tr key={index} className="h-[60px] font-medium">
+                      <td className="w-[17rem]">{product.name}</td>
+                      <td className="w-[10rem]">{product.brand}</td>
+                      <td className="w-[12rem]">{product.category}</td>
+                      <td className="w-[10rem]">{product.price}</td>
+                      <td className="w-[10rem]">
+                        <h2 className={`font-semibold ${product.status.toLowerCase() === "soldout" ? "text-red-400" : "text-[#0C7523]"}`}>{product.status}</h2>
+                      </td>
+                      <td className="w-[10rem]">{product.quantity}</td>
+                      <td className="w-[10rem]">
+                        <Button className="bg-[#D7904D] text-white px-[20px] py-[10px] capitalize">Add</Button>
+                      </td>
                     </tr>
-                  </thead>
-                  <tbody>
-                    {TABLE_ROWS.map(({ name, brand, category, price, status, quantity }, index) => {
-                      const isLast = index === TABLE_ROWS.length - 1;
-                      const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
-
-                      return (
-                        <tr key={name} className="overflow-y-scroll">
-                          <td className={classes}>
-                            <div className="flex items-center">
-                              <Typography variant="small" color="blue-gray" className="font-bold">
-                                {name}
-                              </Typography>
-                            </div>
-                          </td>
-                          <td className={classes}>
-                            <Typography variant="small" color="blue-gray" className="font-normal">
-                              {brand}
-                            </Typography>
-                          </td>
-                          <td className={classes}>
-                            <Typography variant="small" color="blue-gray" className="font-normal">
-                              {category}
-                            </Typography>
-                          </td>
-                          <td className={classes}>
-                            <Typography variant="small" color="blue-gray" className="font-normal">
-                              {price}
-                            </Typography>
-                          </td>
-                          <td className={classes}>
-                            <div className="w-max">
-                              <Chip size="sm" variant="ghost" value={status} color={status === "ready" ? "green" : status === "seol" ? "red" : "red"} />
-                            </div>
-                          </td>
-                          <td className={classes}>
-                            <Typography variant="small" color="blue-gray" className="font-normal">
-                              {quantity}
-                            </Typography>
-                          </td>
-                          <td className={classes}>
-                            <Tooltip content="Add to cart">
-                              <Button className="bg-[#D7904D]">Add</Button>
-                            </Tooltip>
-                          </td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
-              </CardBody>
-              <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-0">
-                <Button variant="outlined" size="sm">
-                  Previous
-                </Button>
-                <div className="flex items-center gap-2">
-                  <IconButton variant="outlined" size="sm">
-                    1
-                  </IconButton>
-                  <IconButton variant="text" size="sm">
-                    2
-                  </IconButton>
-                  <IconButton variant="text" size="sm">
-                    3
-                  </IconButton>
-                  <IconButton variant="text" size="sm">
-                    ...
-                  </IconButton>
-                  <IconButton variant="text" size="sm">
-                    8
-                  </IconButton>
-                  <IconButton variant="text" size="sm">
-                    9
-                  </IconButton>
-                  <IconButton variant="text" size="sm">
-                    10
-                  </IconButton>
-                </div>
-                <Button variant="outlined" size="sm">
-                  Next
-                </Button>
-              </CardFooter>
-            </Card>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
